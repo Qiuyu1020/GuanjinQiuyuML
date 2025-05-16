3781df3c45fba81108702f49e0f35800862e91b3
Qiuyu Meng <z5616539@ad.unsw.edu.au>
2025-05-16 21:50:16 +0800

    data preprocessing: fill ions and HA and NOM area 0

--- k_prediction/data_preprocessing/fill_ions_0.py
+++ k_prediction/data_preprocessing/fill_ions_0.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 
-df = pd.read_excel("PollutantEncode.xlsx")
+df = pd.read_excel("dataset_filled_elements_0.xlsx")
 
 
-target_columns = ["Cl-", "HCO3-", "HPO42-", "NO3-", "HA"]
+target_columns = ["Cl-(mM)", "HCO3-(mM)", "HPO42-(mM)", "NO3-(mM)", "HA", "NOM"]
 
 
 for col in target_columns:
@@ -12,6 +12,6 @@ for col in target_columns:
         df[col] = df[col].fillna(0)
 
 
-df.to_excel("kdatafill0.xlsx", index=False)
+df.to_excel("dataset_filled_elements_ions_0.xlsx", index=False)
 
-print("save as kdatafill0.xlsx")
+print("saved as dataset_filled_elements_ions_0.xlsx")
