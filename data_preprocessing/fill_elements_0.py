eee0a41a2ff914abddfdeb76c7a274a249cc08f4
Qiuyu Meng <z5616539@ad.unsw.edu.au>
2025-05-16 15:12:07 +0800

    data pre-processing: set all NaNs in elements area as 0

---
+++ data_preprocessing/fill_elements_0.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+# read data
+df = pd.read_excel("元素填0缩放.xlsx")
+
+# all elements 118
+basic_elements = {
+    'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
+    'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar',
+    'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn',
+    'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr',
+    'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd',
+    'In', 'Sn', 'Sb', 'Te', 'I', 'Xe',
+    'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy',
+    'Ho', 'Er', 'Tm', 'Yb', 'Lu',
+    'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg',
+    'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn',
+    'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf',
+    'Es', 'Fm', 'Md', 'No', 'Lr',
+    'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Fl', 'Lv', 'Ts', 'Og'
+}
+
+# set a copy of dataset
+df_processed = df.copy()
+
+# loop
+for col in df.columns:
+    if col in basic_elements:
+        # fill all NaN as 0
+        df_processed[col] = df[col].fillna(0)
+        # >1? TRUE->/100 FALSE->remain same
+        df_processed[col] = df_processed[col].apply(lambda x: x / 100 if x > 1 else x)
+
+# save as new xlsx file
+df_processed.to_excel("元素填0缩放.xlsx", index=False)
+print("saved as：元素填0缩放1.xlsx")
